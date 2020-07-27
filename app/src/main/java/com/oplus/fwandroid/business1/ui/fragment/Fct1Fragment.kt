d5928525b9f5c8a2de909e4321df5f43478fdb32
wanganan <waa182838@sina.com>
2020-07-27 18:17:26 +0800

    ""

---
+++ app/src/main/java/com/oplus/fwandroid/business1/ui/fragment/Fct1Fragment.kt
@@ -0,0 +1,12 @@
+package com.oplus.fwandroid.business1.ui.fragment
+
+/**
+ * @author Sinaan
+ * @date 2020/7/27
+ * GitHub：https://github.com/wanganan
+ * email：waa182838@sina.com
+ * description：
+ * version: 1.0
+ */
+class Fct1Fragment {
+}
